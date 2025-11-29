100707a75ac6b6b7e61589d265e021fdc20ca216
PolyVector <PolyVector@protonmail.com>
2025-11-29 09:56:21 +0000

    functional

--- src/backend/compiler/generator.rs
+++ src/backend/compiler/generator.rs
@@ -55,7 +55,7 @@ fn get_operand(instruction: &Instruction, program: &Program) -> Result<u8, Inval
         Instruction::Branch(number_or_identifier)
         | Instruction::BranchZero(number_or_identifier)
         | Instruction::BranchPositive(number_or_identifier) => match number_or_identifier {
-            NumberOrIdentifier::Number(number) => Ok(*number as u8), // TODO: error if > 255
+            NumberOrIdentifier::Number(number) => Ok(*number as u8), // TODO: error if > 99
             NumberOrIdentifier::Identifier(identifier) => {
                 let label = program.labels.get(identifier).ok_or(InvalidIdentifier {
                     identifier: identifier.to_owned(),
@@ -68,22 +68,27 @@ fn get_operand(instruction: &Instruction, program: &Program) -> Result<u8, Inval
         Instruction::Add(number_or_identifier)
         | Instruction::Sub(number_or_identifier)
         | Instruction::Store(number_or_identifier)
-        | Instruction::Load(number_or_identifier) => match number_or_identifier {
-            NumberOrIdentifier::Number(number) => Ok(*number as u8), // TODO: ditto error
-            NumberOrIdentifier::Identifier(identifier) => {
-                let mut number = None;
-
-                for (i, instruction) in program.instructions.iter().enumerate() {
-                    if let Instruction::Data(label, _) = instruction
-                        && label == identifier
-                    {
-                        number = Some(i as u8);
-                    }
-                }
-
-                Ok(number.unwrap())
+        | Instruction::Load(number_or_identifier) => {
+            match number_or_identifier {
+                NumberOrIdentifier::Number(number) => Ok(*number as u8), // TODO: ditto error
+                NumberOrIdentifier::Identifier(identifier) => program
+                    .instructions
+                    .iter()
+                    .enumerate()
+                    .fold(None, |_, (i, instruction)| {
+                        if let Instruction::Data(label, _) = instruction
+                            && label == identifier
+                        {
+                            Some(i as u8)
+                        } else {
+                            None
+                        }
+                    })
+                    .ok_or(Err(InvalidIdentifier {
+                        identifier: Rc::clone(identifier),
+                    })?),
             }
-        },
+        }
 
         _ => unreachable!(),
     }
