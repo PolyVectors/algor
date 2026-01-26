a454671cdfd7045988d0cb04ba71152dda3b2444
PolyVector <PolyVector@protonmail.com>
2026-01-26 09:43:40 +0000

    comment compile function

--- src/backend/compiler.rs
+++ src/backend/compiler.rs
@@ -10,6 +10,7 @@ use crate::backend::compiler::lexer::Lexer;
 use crate::backend::compiler::parser::Parser;
 use std;
 
+// Combines the lexer, parser, and code generator, returning machine code that can be placed into RAM, or a generic error if any method fails
 pub fn compile(source: &str) -> Result<[Location; 100], Box<dyn std::error::Error>> {
     Ok(<[Location; 100]>::try_from(
         Parser::new(Lexer::new(source).lex()?).parse()?,
