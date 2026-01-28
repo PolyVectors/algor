071bac449ada3104827d79ec967e4eae2eade4f1
PolyVector <PolyVector@protonmail.com>
2026-01-28 22:17:57 +0000

    more tests

--- src/backend/compiler/tests.rs
+++ src/backend/compiler/tests.rs
@@ -4,13 +4,13 @@ use crate::backend::compiler::{
     self,
     generator::Location,
     lexer::{InvalidCharacter, Lexer, Token},
-    parser::{InvalidToken, Parser, ParserError, Program},
+    parser::{Instruction, InvalidToken, Operand, Parser, ParserError, Program},
 };
 
 #[test]
 // 1.1.1
 fn lexer_all_tokens() {
-    let source = "HLT COB ADD SUB STA STO LDA BRA BRZ BRP INP OUT DAT 189 ABYZ abyz\n";
+    let source = "HLT COB ADD SUB STA STO LDA BRA BRZ BRP INP OUT DAT 189 -189 ABYZ abyz\n";
 
     assert_eq!(
         Lexer::new(source).lex(),
@@ -82,8 +82,27 @@ fn parser_all_instructions() {
     assert_eq!(
         Parser::new(Lexer::new(source).lex().unwrap()).parse(),
         Ok(Program {
-            labels: HashMap::new(),
-            instructions: Vec::new(),
+            labels: {
+                let mut labels = HashMap::new();
+                labels.insert("yzab".into(), 6);
+                labels
+            },
+            instructions: vec![
+                Instruction::Halt,
+                Instruction::Halt,
+                Instruction::Add(Operand::Number(19)),
+                Instruction::Sub(Operand::Identifier("ABYZ".into())),
+                Instruction::Store(Operand::Identifier("ABYZ".into())),
+                Instruction::Store(Operand::Number(19)),
+                Instruction::Load(Operand::Identifier("ABYZ".into())),
+                Instruction::Branch(Operand::Identifier("yzab".into())),
+                Instruction::BranchZero(Operand::Identifier("yzab".into())),
+                Instruction::BranchPositive(Operand::Identifier("yzab".into())),
+                Instruction::Input,
+                Instruction::Output,
+                Instruction::Data("ABYZ".into(), 19),
+                Instruction::Data("YZAB".into(), 0),
+            ],
         })
     );
 }
@@ -103,7 +122,7 @@ fn parser_too_many_tokens() {
 }
 
 #[test]
-//1.2.3
+//1.2.2
 fn parser_not_enough_tokens() {
     let source = "ADD";
 
@@ -116,14 +135,46 @@ fn parser_not_enough_tokens() {
     );
 }
 
-// TODO: formatting test, invalid identifier test
+#[test]
+//1.2.3
+fn parser_address_out_of_range() {
+    let source = "ADD 100";
+
+    assert_eq!(
+        Parser::new(Lexer::new(source).lex().unwrap()).parse(),
+        Err(ParserError::AddressOutOfRange(100))
+    );
+}
+
+#[test]
+//1.2.4
+fn parser_number_out_of_range() {
+    let source = "A DAT 1000";
+
+    assert_eq!(
+        Parser::new(Lexer::new(source).lex().unwrap()).parse(),
+        Err(ParserError::NumberOutOfRange(1000))
+    );
+}
 
 #[test]
 //1.3.1
 fn generator_all_instructions() {
-    let source = r#"loop ADD num
-        BRA loop
-        num DAT 999"#;
+    let source = r#"HLT
+        COB
+        ADD 19
+        SUB ABYZ
+        STA ABYZ
+        STO 19
+        yzab LDA ABYZ
+        BRA yzab
+        BRZ yzab
+        BRP yzab
+        INP
+        OUT
+        ABYZ DAT 19
+        YZAB DAT
+        "#;
 
     assert_eq!(compiler::compile(source).unwrap(), [Location::Data(0); 100]);
 }
