73155f2252541bcc84561cb71d36dbdd34ac7608
PolyVector <PolyVector@protonmail.com>
2026-02-12 05:29:36 +0000

    docs(lesson_view): comments

--- src/frontend/screen/lesson_view.rs
+++ src/frontend/screen/lesson_view.rs
@@ -20,28 +20,42 @@ use iced::{
     widget::{button, column, container, pane_grid, row, space, text, text_editor},
 };
 
+// Messages specific to lesson view screen
 #[derive(Debug, Clone)]
 pub enum Message {
+    // Events related to the pane widget
     PaneClicked(pane_grid::Pane),
     PaneDragged(pane_grid::DragEvent),
     PaneResized(pane_grid::ResizeEvent),
+    // Text editor pane messages
     Editor(editor::Message),
+    // State viewer pane message
     StateViewer(state_viewer::Message),
+    // Terminal pane message
     Terminal(terminal::Message),
+    // Back button in lesson pane clicked
     BackLessonClicked,
+    // Next button in lesson pane clicked
     NextLessonClicked,
     BackClicked,
     SettingsClicked,
 }
 
+// Events specific to lesson view screen
 pub enum Event {
+    // Result from clicking run button in editor pane
     Run,
+    // Ditto for stop button
     Stop,
+    // Ditto for reset button
     Reset,
+    // Result from clicking back button
     ToLessonSelect,
+    // Result from clicking settings button
     ToSettings,
 }
 
+// Represents individual panes in the screen
 #[derive(Debug, Clone)]
 pub enum Pane {
     Editor,
@@ -53,30 +67,42 @@ pub enum Pane {
 #[derive(Debug, Clone)]
 pub struct State {
     pub lesson: Lesson,
+    // Whether or not a lesson has been completed
     pub completed: bool,
+    // The current slide (of a lesson) the user is on
     pub slide: usize,
+    // The index of the next input
     pub input: usize,
     panes: pane_grid::State<Pane>,
+    // Current pane the user is interacting with
     pane_focused: Option<pane_grid::Pane>,
+    // Text content of the text editor pane
     content: text_editor::Content,
     pub text_size: u32,
     pub computer: Arc<Mutex<Computer>>,
     sender: Arc<Mutex<Sender<Input>>>,
+    // Terminal pane outputs
     pub output: Vec<Box<str>>,
+    // Terminal pane error
     pub error: String,
 }
 
 impl State {
+    // Constructor for the lesson viewer
     pub fn new(
         lesson: Lesson,
         computer: Arc<Mutex<Computer>>,
         sender: Arc<Mutex<Sender<Input>>>,
         text_size: u32,
     ) -> Self {
+        // Start with editor pane
         let (mut panes, pane) = pane_grid::State::new(Pane::Editor);
 
+        // Split vertically (editor pane on left, lesson pane on the right)
         panes.split(pane_grid::Axis::Vertical, pane, Pane::Lesson);
+        // Split horizontally (editor pane om top, state viewer pane below, lesson pane on the right)
         panes.split(pane_grid::Axis::Horizontal, pane, Pane::StateViewer);
+        // Split horizontally again (editor pane on top, followed by terminal and state viewer below, lesson pane on the right)
         panes.split(pane_grid::Axis::Horizontal, pane, Pane::Terminal);
 
         Self {
@@ -99,6 +125,7 @@ impl State {
 impl State {
     pub fn update(&mut self, message: Message) -> Option<Event> {
         match message {
+            // Pane interactivity boilerplate
             Message::PaneClicked(pane) => {
                 self.pane_focused = Some(pane);
             }
@@ -115,6 +142,7 @@ impl State {
                 editor::Message::AssembleClicked => {
                     self.error = String::new();
 
+                    // Send a message to the sender to compile the program with the source code in the text editor
                     if let Ok(mut sender) = self.sender.lock() {
                         // TODO: stop unwrapping
                         sender
@@ -134,6 +162,7 @@ impl State {
             Message::BackClicked => return Some(Event::ToLessonSelect),
 
             Message::NextLessonClicked => {
+                // Only allow the user to progress to the next lesson if the outputs match the list of outputs defined in the lesson
                 if self
                     .output
                     .iter()
@@ -142,6 +171,7 @@ impl State {
                     == self.lesson.body.slides[self.slide].outputs.items
                     && self.error == String::new()
                 {
+                    // Complete the lesson if there are no more slides
                     if self.slide < self.lesson.body.slides.len() - 1 {
                         self.slide += 1
                     } else {
@@ -166,6 +196,7 @@ impl State {
                 pane_grid(&self.panes, |pane, state, _is_maximized| {
                     let focused = self.pane_focused == Some(pane);
 
+                    // Convert Pane to static string
                     let title = match state {
                         Pane::Editor => "Editor",
                         Pane::StateViewer => "State Viewer",
@@ -173,6 +204,7 @@ impl State {
                         Pane::Lesson => "Lesson",
                     };
 
+                    // Add title to title bar
                     let title_bar = pane_grid::TitleBar::new(
                         container(text(title)).padding([4, 8]),
                     )
@@ -188,7 +220,6 @@ impl State {
                         }
 
                         Pane::StateViewer => {
-                            // TODO: stop unwrapping
                             state_viewer(&self.computer.lock().unwrap()).map(Message::StateViewer)
                         }
 
@@ -197,6 +228,7 @@ impl State {
                         }
 
                         Pane::Lesson => column![
+                            // Show lesson slide content with navigation buttons if the lesson is not completed
                             (!self.completed).then(|| {
                                 container(column![
                                     self.lesson.body.slides[self.slide].parse(),
@@ -208,9 +240,11 @@ impl State {
                                     ]
                                 ])
                             }),
+                            // ... otherwise show some text indicating the lesson is complete
                             self.completed
                                 .then(|| { text("Lesson Completed!").font(Font::Bold).size(24) }),
                         ]
+                        // Padding as to not cause elements to appear behind the title bar
                         .padding(Padding {
                             left: 8f32,
                             top: 6f32,
@@ -224,9 +258,11 @@ impl State {
                     } else {
                         style::grid_pane_unfocused
                     })
+                    // Set title bar to previously defined title_bar variable
                     .title_bar(title_bar)
                 })
                 .spacing(8)
+                // Connect boilerplate pane interactivity Message variants
                 .on_click(Message::PaneClicked)
                 .on_drag(Message::PaneDragged)
                 .on_resize(10, Message::PaneResized)
