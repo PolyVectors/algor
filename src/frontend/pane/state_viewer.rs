297926e118459ef63e006c5e056c85c920e31fee
PolyVector <PolyVector@protonmail.com>
2026-02-14 02:32:00 +0000

    docs(state_viewer & terminal): comments and underline bug fix

--- src/frontend/pane/state_viewer.rs
+++ src/frontend/pane/state_viewer.rs
@@ -8,6 +8,7 @@ use crate::{
     shared::vm::Computer,
 };
 
+// No messages required but provide a mapping for future maintainability
 #[derive(Debug, Clone)]
 pub enum Message {}
 
@@ -19,30 +20,35 @@ pub fn state_viewer<'a>(computer: &Computer) -> Element<'a, Message> {
                 separator::horizontal(),
                 row![
                     column![
+                        // Force PC register display to be 2 digits wide
                         text(format!("{:02}", computer.program_counter)),
                         text("PC").size(12)
                     ]
                     .align_x(alignment::Horizontal::Center)
                     .width(Length::Fixed(45f32)),
                     column![
+                        // Force accumulator register display to be 4 digits wide (as to be mindful for plus and minus sign prefixes)
                         text(format!("{:04}", computer.accumulator)),
                         text("ACC").size(12)
                     ]
                     .align_x(alignment::Horizontal::Center)
                     .width(Length::Fixed(45f32)),
                     column![
+                        // CIR register only needs to be one digit wide as there are less than 10 opcodes in the LMC ISA
                         text(format!("{:01}", computer.current_instruction_register)),
                         text("CIR").size(12)
                     ]
                     .align_x(alignment::Horizontal::Center)
                     .width(Length::Fixed(45f32)),
                     column![
+                        // 100 memory addresses starting from 0 (i.e. highest address is 99) so only 2 digits are required
                         text(format!("{:02}", computer.memory_address_register)),
                         text("MAR").size(12)
                     ]
                     .align_x(alignment::Horizontal::Center)
                     .width(Length::Fixed(45f32)),
                     column![
+                        // MDR only contains blah blah
                         text(format!("{:04}", computer.memory_data_register)),
                         text("MDR").size(12)
                     ]
@@ -52,8 +58,13 @@ pub fn state_viewer<'a>(computer: &Computer) -> Element<'a, Message> {
                 .spacing(16),
                 text("RAM:"),
                 separator::horizontal(),
+                // Display all memory addresses with indexes, underline the address currently in use by the program counter
                 row(computer.memory.iter().enumerate().map(|(i, value)| column![
-                    rich_text![span::<(), Font>(format!("{value}")).underline(i as u8 == 0)],
+                    // Use rich text for underline feature
+                    rich_text![
+                        span::<(), Font>(format!("{value}"))
+                            .underline(i as u8 == computer.program_counter)
+                    ],
                     text(format!("{i}")).size(8)
                 ]
                 .width(Length::Fixed(45f32))
@@ -65,10 +76,12 @@ pub fn state_viewer<'a>(computer: &Computer) -> Element<'a, Message> {
             .padding(6)
             .spacing(16),
         )
+        // Use a style that forces background to render for elements with scrollbars (ditto src/frontend/pane/editor.rs solid_background comment)
         .style(style::background_scrollable)
         .width(Length::Fill)
         .height(Length::Fill),
     )
+    // Padding to avoid elements rendering under title bar
     .padding(Padding {
         top: 0f32,
         right: 2f32,
--- src/frontend/pane/terminal.rs
+++ src/frontend/pane/terminal.rs
@@ -5,18 +5,21 @@ use iced::{
 
 use crate::frontend::pane::style;
 
+// White text
 pub fn terminal_out(_theme: &Theme) -> text::Style {
     text::Style {
         color: Some(Color::from_rgb(1f32, 1f32, 1f32)),
     }
 }
 
+// Red text
 pub fn terminal_err(_theme: &Theme) -> text::Style {
     text::Style {
         color: Some(Color::from_rgb(1f32, 0f32, 0f32)),
     }
 }
 
+// No messages required but provide a mapping for future maintainability
 #[derive(Debug, Clone)]
 pub enum Message {}
 
@@ -25,10 +28,15 @@ pub fn terminal<'a>(output: &'a Vec<Box<str>>, error: &'a String) -> Element<'a,
         scrollable(
             column![
                 column(
+                    // For every output display text in pure white
                     output
                         .iter()
+                        /* Dereference output (i.e. get Box<str>)
+                         Dereference again to unbox (i.e. get str)
+                         And take a reference again as the size of str by itself is unknown at compile time (i.e. get &str) */
                         .map(|output| { text(&**output).style(terminal_out).into() })
                 ),
+                // Show errors below output
                 text(error).style(terminal_err)
             ]
             .padding(6)
